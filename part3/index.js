b64b03dab8753c7fb36c1684c0a96137021556e0
ikonuk <28847139+ibrahimkonuk@users.noreply.github.com>
2022-02-08 22:03:52 +0100

    Complete Exercises 3.1.-3.6.

---
+++ part3/index.js
@@ -0,0 +1,86 @@
+const { request } = require('express');
+const express = require('express');
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+let phonebook = [
+    {
+        "id": 1,
+        "name": "Arto Hellas",
+        "number": "040-123456"
+    },
+    {
+        "id": 2,
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
+    },
+    {
+        "id": 3,
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": 4,
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    }
+]
+
+app.get('/', (request, response) => {
+    response.send('Phonebook app')
+})
+
+app.get('/api/phonebook', (request, response) => {
+    response.json(phonebook)
+})
+
+app.get('/api/phonebook/:id', (request, response) => {
+
+    let id = Number(request.params.id)
+    const contact = phonebook.filter(c => c.id === id)
+    if (contact.length) {
+        response.json(contact)
+    } else {
+        response.status(404).json({
+            error: 'contact not found'
+        }).end()
+    }
+})
+
+app.delete('/api/phonebook/:id', (request, response) => {
+    let id = Number(request.params.id)
+    phonebook = phonebook.filter(contact => contact.id !== id)
+    response.status(204).end()
+})
+
+app.post('/api/phonebook', (request, response) => {
+
+    let duplicate = phonebook
+        .some(c =>
+            c.name.toLowerCase() === request.body.name.trim().toLowerCase())
+
+    if (duplicate)
+        response.status(409).json({ error: 'contact already exists' })
+
+    if (!request.body.name || !request.body.number)
+        response.status(400).json({ error: 'empty parameter' }).end()
+
+    const contact = {
+        ...request.body,
+        'id': Math.floor(Math.random() * 1000)
+    }
+    phonebook.concat(contact)
+    response.json(contact)
+})
+
+app.get('/api/info', (request, response) => {
+    let num = phonebook.length
+    let now = new Date()
+    response.send(`Phonebook has info for ${num} people. \n\n${now}`)
+})
+
+const PORT = 3001
+app.listen(PORT)
+console.log(`Server running on port ${PORT}`)
