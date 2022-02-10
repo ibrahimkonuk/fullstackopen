4b4a8554395db7da6bfc73c9b62434cbabab8404
ikonuk <28847139+ibrahimkonuk@users.noreply.github.com>
2022-02-10 21:59:39 +0100

    Complete Exercises 3.15.-3.18.

--- part3/index.js
+++ part3/index.js
@@ -17,54 +17,86 @@ app.get('/', (request, response) => {
     response.send('Phonebook app')
 })
 
-app.get('/api/phonebook', (request, response) => {
+app.get('/api/phonebook', (request, response, next) => {
     Contact.find({})
-        .then(notes => {
-            response.json(notes)
+        .then((notes) => {
+            notes
+                ? response.json(notes)
+                : response.status(404)
         })
-        .catch(() => {
-            response.status(204).json({ message: "phonebook is empty" })
-        })
+        .catch((error) => next(error))
 })
 
-app.get('/api/phonebook/:id', (request, response) => {
+app.get('/api/phonebook/:id', (request, response, next) => {
     Contact.find({ _id: request.params.id })
         .then((contact) => {
-            response.json(contact)
+            contact
+                ? response.json(contact)
+                : response.status(404)
         })
+        .catch((error) => next(error))
 })
 
-app.delete('/api/phonebook/:id', (request, response) => {
+app.delete('/api/phonebook/:id', (request, response, next) => {
     Contact.findByIdAndRemove({ _id: request.params.id })
         .then(() => {
-            response.status(200).json({ success: 'contact deleted' })
+            response.status(200).send({ success: 'contact deleted' })
         })
-        .catch(() => response.status(500).json({ error: "couldn't delete" }))
+        .catch((error) => next(error))
 })
 
-app.post('/api/phonebook', (request, response) => {
+app.put('/api/phonebook/:id', (request, response, next) => {
+    Contact.findByIdAndUpdate(request.params.id, request.body, { new: true })
+        .then((updatedNote) => {
+            updatedNote
+                ? response.json(updatedNote)
+                : response.status(404)
+        })
+        .catch((error) => next(error))
+})
+
+app.post('/api/phonebook', (request, response, next) => {
     let body = request.body
 
     if (!body.name || !body.phone)
-        response.status(400).json({ error: 'empty parameter' })
+        response.status(400).send({ error: 'empty parameter' })
 
     Contact.exists({ phone: body.phone })
         .then((contact) => {
             if (contact) {
-                response.status(409).json({ error: 'phone number already exists' })
+                response.status(409).send({ error: 'phone number already exists' })
             } else {
                 Contact.create(body)
                 response.status(200).json(body);
             }
         })
+        .catch((error) => next(error))
 })
 
-app.get('/api/info', (request, response) => {
+app.get('/api/info', (request, response, next) => {
     let now = new Date()
     Contact.countDocuments()
         .then(numOfContacts => response.send(`Phonebook has info for ${numOfContacts} people. \n\n${now}`))
-        .catch(() => response.status(500).json({ error: "couldn't get info" }))
+        .catch((error) => next(error))
 })
+
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+// handler of requests with unknown endpoint
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+
+    next(error)
+}
+// handler of requests with result to errors
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
